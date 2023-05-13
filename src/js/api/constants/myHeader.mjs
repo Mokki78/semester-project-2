b7afa15a9e79c42fe8d869042c15fbb7f462ead9
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2023-05-13 17:06:31 +0200

    fixed mobile menu, footer and main menu

---
+++ src/js/api/constants/myHeader.mjs
@@ -0,0 +1,17 @@
+
+import { load } from '../localStorage';
+
+
+export const headers = (contentType) => {
+    const token = storage.load("token");
+    const headers= {}
+
+    if (contentType) {
+        headers["Content-Type"] = contentType;
+    }
+    if(token) {
+        headers.Authorization = 'Bearer ${token}';
+    } 
+    
+    return headers;
+}
