6ceb16261a89a7f3c39908f757c93e860274030f
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2023-05-15 13:18:04 +0200

    working searchbar on desktop

--- src/js/search params/search.mjs
+++ src/js/search params/search.mjs
@@ -1,40 +1,48 @@
- import { API_BASE_URL} from "../api/constants/apiBase.mjs";
- import { headers } from "../api/constants/headers.mjs";
-
+import { API_BASE_URL } from "../api/constants/apiBase.mjs";
+import { headers } from "../api/constants/headers.mjs";
 
 async function fetchSearchContent() {
   try {
     const headersObject = headers();
-    const response = await fetch(`${API_BASE_URL}/auction/listings`, { headers: headersObject });
+    const response = await fetch(`${API_BASE_URL}/auction/listings/?title`, { headers: headersObject });
 
     if (response.ok) {
       const listings = await response.json();
 
-      const searchInput = document.querySelector('.search-options').value.toLowerCase();
+      const searchButton = document.querySelector(".searchBtn");
+      searchButton.addEventListener("click", () => {
+        const searchInput = document.querySelector('.search-options').value.toLowerCase();
 
-      const filteredListings = listings.filter(listing => {
-        const { title, description } = listing;
-        return title.toLowerCase().includes(searchInput) || description.toLowerCase().includes(searchInput);
-      });
-
-      const searchContainer = document.querySelector("#search-content");
-      searchContainer.innerHTML = '';
-
-      if (filteredListings.length === 0) {
-        profileContainer.innerHTML = 'No matching auction found.';
-      } else {
-        filteredListings.forEach(listing => {
-          const { title, media } = listing;
-
-          const listingElement = document.createElement('div');
-          listingElement.innerHTML = `
-            <h1>${title}</h1>
-            <img src="${media}" alt="Listing Image">
-          `;
-
-          searchContainer.appendChild(listingElement);
+        const filteredListings = listings.filter(listing => {
+          const { title } = listing;
+          return title.toLowerCase().includes(searchInput);
         });
-      }
+
+        const searchContainer = document.querySelector(".search-content");
+        searchContainer.innerHTML = '';
+       
+
+
+        if (filteredListings.length === 0) {
+          searchContainer.innerHTML = 'No matching auction found.';
+          alert("No auction matching your search");
+         
+      
+          } else {
+          filteredListings.forEach(listing => {
+            const { title, media } = listing;
+
+            const listingElement = document.createElement('div');
+            listingElement.innerHTML = `
+              <h1>${listing.title}</h1>
+              <img src="${listing.media}" alt="Listing Image">
+            `;
+
+            searchContainer.appendChild(listingElement);
+         
+          });
+        }
+      });
     } else {
       throw new Error(response.statusText);
     }
@@ -42,10 +50,9 @@ async function fetchSearchContent() {
     console.error('Error fetching listings:', error);
     throw error;
   }
-
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  fetchSearchContent() ;
+  fetchSearchContent();
 });
 
-}
